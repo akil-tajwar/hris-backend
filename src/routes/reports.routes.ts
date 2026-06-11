e95200f58a7d22cc958185ee82bc9ba3bf39b06c
Akil Tajwar <akiltajwar@gmail.com>
2026-06-11 13:30:58 +0600

    feat: employee activity report api

--- src/routes/reports.routes.ts
+++ src/routes/reports.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 
-import { employeeAttendanceReportController, loneReportController, salaryReportController } from '../controllers/reports.controller'
+import { employeeActivitiesReportController, employeeAttendanceReportController, loneReportController, salaryReportController } from '../controllers/reports.controller'
 import { authenticateUser } from '../middlewares/auth.middleware'
 
 const router = Router()
 
+router.get('/activity-report/:employeeId', authenticateUser, employeeActivitiesReportController)
 router.get('/attendance-report', authenticateUser, employeeAttendanceReportController)
 router.get('/salary-report', authenticateUser, salaryReportController)
 router.get('/lone-report', authenticateUser, loneReportController)
